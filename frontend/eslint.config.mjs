835620e74b4420f4b8c91008b2c94438ae9f2e11
premierbell <pjw319@naver.com>
2025-11-27 11:33:57 +0900

    vercel용 수정

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -19,6 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "react-hooks/exhaustive-deps": "warn"
+    }
   },
 ];
 
